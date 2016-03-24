f0b71a21f194b60ad6946e89661b7343821b3550
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-03-24 17:46:42 +0900

    アプリからライブラリを使えるよう修正

--- app/src/main/java/com/example/t_sei/touka_sdk/MainActivity.java
+++ app/src/main/java/com/example/t_sei/touka_sdk/MainActivity.java
@@ -5,15 +5,22 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.t_sei.touka.Touka;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        String st = Touka.taishio();
+        Log.d("OSA", st);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
--- touka/src/main/java/com/example/t_sei/touka/Touka.java
+++ touka/src/main/java/com/example/t_sei/touka/Touka.java
@@ -6,7 +6,7 @@ import android.util.Log;
  * Created by t-sei on 16/03/24.
  */
 public class Touka {
-    static String taishio() {
+    public static String taishio() {
         if ( BuildConfig.DEBUG){
             Log.d("OSA", "hosomen saiko");
         }
